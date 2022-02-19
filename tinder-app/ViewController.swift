ac63628873bdbb52405229195badc8f2649f5124
H-Yuuki <yuukirinnma@gmail.com>
2022-02-19 22:53:30 +0900

    add: 縦並び3つの画面を作成

--- tinder-app/ViewController.swift
+++ tinder-app/ViewController.swift
@@ -12,8 +12,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        // 3画面の描画
+        // 上画面
+        let topView = UIView()
+        topView.backgroundColor = .yellow
+        // 中央画面
+        let centerView = UIView()
+        centerView.backgroundColor = .blue
+        // 下画面
+        let bottomView = UIView()
+        bottomView.backgroundColor = .green
+        
+        // 画面を並べる
+        let stackView = UIStackView(arrangedSubviews: [topView, centerView, bottomView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false   // 配置を自動で調整する
+        // 3画面を均等に配置する
+        stackView.axis = .vertical
+        
+        self.view.addSubview(stackView)   // 作成したViewを描画
+        
+        // view の大きさや位置を指定
+        [
+            // 上下の画面の高さを指定
+            topView.heightAnchor.constraint(equalToConstant: 100),
+            bottomView.heightAnchor.constraint(equalToConstant: 100),
+            
+            // 上下左右の余白を無しにする, safeArea の領域を考慮して配置
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ]
+            .forEach { $0.isActive = true }
     }
 
 
