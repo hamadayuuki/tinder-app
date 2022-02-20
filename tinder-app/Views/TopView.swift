739e50804778db26558fbf6e42874ccb09c72dc3
H-Yuuki <yuukirinnma@gmail.com>
2022-02-20 20:51:25 +0900

    add: RxSwift を使用して、アイコンの描画を選択の有無に応じて変更

--- tinder-app/Views/TopView.swift
+++ tinder-app/Views/TopView.swift
@@ -6,18 +6,23 @@
 //
 
 import UIKit
+import RxCocoa   // ボタンを押した時の処理
+import RxSwift
 
 // ホーム画面上 の画面
 class TopView: UIView {
     
-    let tinderButton = createToButton(imageName: "tinder-select")
-    let goodButton = createToButton(imageName: "good-select")
-    let commentButton = createToButton(imageName: "comment-select")
-    let profileButton = createToButton(imageName: "profile-select")
+    let disposeBag = DisposeBag()   // ボタンを押した時の処理で使用
+    
+    let tinderButton = createToButton(imageName: "tinder")
+    let goodButton = createToButton(imageName: "good")
+    let commentButton = createToButton(imageName: "comment")
+    let profileButton = createToButton(imageName: "profile")
     
     static private func createToButton(imageName: String) -> UIButton {
         let button = UIButton(type: .custom)
-        button.setImage(UIImage(named: imageName), for: .normal)
+        button.setImage(UIImage(named: imageName), for: .selected)   // UIButton.isSelected == true の時
+        button.setImage(UIImage(named: "\(imageName)-unselect"), for: .normal)
         button.imageView?.contentMode = .scaleAspectFit
         return button
     }
@@ -25,6 +30,12 @@ class TopView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        setupLayout()
+        setupBinding()
+        
+    }
+    
+    private func setupLayout() {
         let baseHorizonStackView = UIStackView(arrangedSubviews: [tinderButton, goodButton, commentButton, profileButton])
         baseHorizonStackView.axis = .horizontal
         baseHorizonStackView.distribution = .fillEqually
@@ -43,6 +54,43 @@ class TopView: UIView {
         ]
             .forEach { $0.isActive = true }
         
+        tinderButton.isSelected = true   // 初期に表示するボタン
+    }
+    
+    // ボタンを押した時の処理
+    private func setupBinding() {
+        
+        // RxSwift を使用してボタンタップを検知
+        tinderButton.rx.tap
+            .subscribe { value in
+                self.changeButtonIsSelected(selectButton: self.tinderButton)   // アイコンの描画に関する変数 の値を変更
+                //print(#function)   // 関数を表示
+            }
+        goodButton.rx.tap
+            .subscribe { value in
+                self.changeButtonIsSelected(selectButton: self.goodButton)
+            }
+        commentButton.rx.tap
+            .subscribe { value in
+                self.changeButtonIsSelected(selectButton: self.commentButton)
+            }
+        profileButton.rx.tap
+            .subscribe { value in
+                self.changeButtonIsSelected(selectButton: self.profileButton)
+            }
+    }
+    
+    // アイコンの表示を切り替えるため、.isSelected の変更
+    private func changeButtonIsSelected(selectButton: UIButton) {
+        let buttons = [self.tinderButton, self.goodButton, self.commentButton, self.profileButton]
+        
+        buttons.forEach { button in
+            if selectButton == button {
+                button.isSelected = true
+            } else {
+                button.isSelected = false
+            }
+        }
     }
     
     required init?(coder: NSCoder) {
