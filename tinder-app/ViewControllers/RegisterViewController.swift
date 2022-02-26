cd8ee1d2d7092d5d7a1bb9bc2fdf5f3272571965
H-Yuuki <yuukirinnma@gmail.com>
2022-02-26 15:42:22 +0900

    add: 要素(TextField, Button)の非同期処理, FireAuth へのユーザー登録

--- tinder-app/ViewControllers/RegisterViewController.swift
+++ tinder-app/ViewControllers/RegisterViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import RxSwift
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
     
     let titleLabel = RegisterTitleLabel()   // タイトル "Tinder"
     let nameTextField = RegisterTextField(placeHolder: "名前")   // ユーザー名
@@ -21,6 +25,7 @@ class RegisterViewController: UIViewController {
         
         setGradientLayout()   // 他の要素よりも早く描画させる
         setLayout()
+        setupBindings()
     }
     
     // グラデーション の描画
@@ -48,5 +53,55 @@ class RegisterViewController: UIViewController {
         nameTextField.anchor(height: 40)   // 1つの高さを指定すると他の大きさも同じ高さになる
         baseHorizontalStackView.anchor(left: view.leftAnchor, right: view.rightAnchor, centerY: view.centerYAnchor, leftPadding: 40, rightPadding: 40)
         titleLabel.anchor(bottom: baseHorizontalStackView.topAnchor, centerX: baseHorizontalStackView.centerXAnchor, bottomPadding: 10)
+    }
+    
+    // 非同期処理
+    private func setupBindings() {
+        nameTextField.rx.text
+            .asDriver()
+            // [weak self]: 循環参照を避ける
+            .drive { [weak self] text in
+                print(text)
+            }
+            .disposed(by: disposeBag)
+        
+        emailTextField.rx.text
+            .asDriver()
+            .drive { [weak self] text in
+                print(text)
+            }
+            .disposed(by: disposeBag)
+        
+        passwordTextField.rx.text
+            .asDriver()
+            .drive { [weak self] text in
+                print(text)
+            }
+            .disposed(by: disposeBag)
+        
+        registerButton.rx.tap
+            .asDriver()
+            .drive { [weak self] text in
+                self?.createUserToFireAuth()
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    // 登録処理
+    private func createUserToFireAuth() {
+        // nilチェック
+        guard let email = emailTextField.text else { return }
+        guard let name = nameTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        // 登録
+        Auth.auth().createUser(withEmail: email, password: password) { (auth, err) in
+            if let err = err {
+                print("登録失敗: ", err)
+            }
+            
+            guard let uid = auth?.user.uid else { return }
+            print("登録成功: ", uid)
+        }
     }
 }
